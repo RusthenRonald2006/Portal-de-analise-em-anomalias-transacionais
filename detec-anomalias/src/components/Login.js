b5af256d405486bca3d5abed1c29d15898bac620
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-03-13 15:13:19 -0300

    importando logo banese

--- detec-anomalias/src/components/Login.js
+++ detec-anomalias/src/components/Login.js
@@ -1,9 +1,17 @@
 import styles from './Login.module.css'
-import Logobanese from '../components/img/logobanese2.png'
+import logobanesefundo from '../components/img/logobaneseofc.png'
+import logobanese from '../components/img/logo banese.png'
 function Login (){
     return(//container
         <div className={styles.login_container}>
-           
+           <div className={styles.login_esquerdo}>
+                
+           </div>
+           <div className={styles.login_direito}>
+
+           </div>
         </div>
     )
 } export default Login
+
+//
