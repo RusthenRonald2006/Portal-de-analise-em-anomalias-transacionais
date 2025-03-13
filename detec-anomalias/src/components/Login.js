57b50e26bc445fff231fd6366fd83c6a5c2155b5
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-13 01:55:36 -0300

    importando logo

--- detec-anomalias/src/components/Login.js
+++ detec-anomalias/src/components/Login.js
@@ -1,9 +1,10 @@
 import styles from './Login.module.css'
+import Logobanese from '../components/img/logo_banese_sem_fundo.png'
 function Login (){
     return(//container
         <div className={styles.login_container}>
             <div className={styles.login_esquerdo}>
-                <div></div>
+                <img></img>
             </div>
             <div className={styles.login_direito}>
 
