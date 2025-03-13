4c94409fe5bc2377a143610812c12e266c8acf0a
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-13 02:06:11 -0300

    criando formulário

--- detec-anomalias/src/components/Login.js
+++ detec-anomalias/src/components/Login.js
@@ -7,7 +7,38 @@ function Login (){
                 <img src={Logobanese} className={styles.logo}></img>
             </div>
             <div className={styles.login_direito}>
+            <div className="login-box">
+                <h2>Welcome to</h2>
+                <h3>Suspicious transactions</h3>
+                <form>
+                    <div className="input-group">
+                    <span className="icon">📧</span>
+                    <input type="email" placeholder="example@gmail.com" required />
+                    </div>
 
+                    <div className="input-group">
+                    <span className="icon">🔒</span>
+                    <input type="password" placeholder="********" required />
+                    </div>
+
+                    <div className="options">
+                    <label>
+                        <input type="checkbox" /> Remember me
+                    </label>
+                    <a href="#">Forgot Password?</a>
+                    </div>
+
+                    <button type="submit" className="login-btn">Login</button>
+
+                    <div className="google-login">
+                    <span>🔵</span> Login with Google
+                    </div>
+
+                    <p className="register-link">
+                    Don’t have an account? <a href="#">Register</a>
+                    </p>
+                </form>
+                </div>
             </div>
         </div>
     )
