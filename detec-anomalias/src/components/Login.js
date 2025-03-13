cc0f7cd0ba6679d423538eb9e787ae21c768c02f
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-13 01:33:50 -0300

    começando estilização da tela de login

--- detec-anomalias/src/components/Login.js
+++ detec-anomalias/src/components/Login.js
@@ -2,10 +2,10 @@ import styles from './Login.module.css'
 function Login (){
     return(//container
         <div className={styles.login_container}>
-            <div>
-
+            <div className={styles.login_esquerdo}>
+                <div></div>
             </div>
-            <div>
+            <div className={styles.login_direito}>
 
             </div>
         </div>
