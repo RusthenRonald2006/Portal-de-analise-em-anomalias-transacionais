8d7e6eaf0107e25bfc573d23be1f41c9fbc0eb84
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-14 15:43:15 -0300

    criando navbar

---
+++ detec-anomalias/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar()[
+    return(
+        <nav>
+            <ul>
+                <li><a>Contas Suspeitas</a></li>
+                <li><a>Contas Suspeitas</a></li>
+                <li><a>Contas Suspeitas</a></li>
+                <li><a>Contas Suspeitas</a></li>
+                <li><a>Contas Suspeitas</a></li>
+            </ul>
+        </nav>
+    )
+] export default Navbar
