e9ebd4ececcb538f832c30ced80c1b5def6a954b
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-14 15:53:02 -0300

    atualizei

--- detec-anomalias/src/components/layout/Navbar.js
+++ detec-anomalias/src/components/layout/Navbar.js
@@ -1,7 +1,8 @@
+import styles from "./Navbar.module.css"
 function Navbar(){
     return(
-        <nav>
-            <ul>
+        <nav className={styles.container_list}>
+            <ul >
                 <li><a>Contas Suspeitas</a></li>
                 <li><a>Gestão de Transações</a></li>
                 <li><a>Gestão de Contas</a></li>
