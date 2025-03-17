0ddc018486d55db1ad465d1b60b212d207252527
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-17 15:42:31 -0300

    adicionando uma nova tela de login para larguras menores

--- detec-anomalias/src/components/Login.js
+++ detec-anomalias/src/components/Login.js
@@ -16,6 +16,40 @@ function Login (){
                <div>
                     <h1 className={`${styles.title_banese} ${mediastyles.titlebanese}`}>Banese</h1>
                </div>
+               <div className={`${mediastyles.login_small_screen}`}>
+               <div className={`${styles.form_container} ${mediastyles.responsive} ${mediastyles.form_section}`}>
+                    <div>
+                        <h1>Welcome to</h1>
+                        <h2>Suspicious transactions</h2>
+                    </div>
+                    <form>
+                        <div className={styles.form_group}>
+                            <label htmlFor="e-mail">Email</label>
+                            <input type='email' name="e-mail" placeholder='Digite seu e-mail'></input>
+                        </div>
+                        <div className={styles.form_group}>
+                            <label htmlFor="password">Senha</label>
+                            <input type='password' name="password" placeholder="Digite sua senha"></input>
+                        </div>
+                        <div className={styles.container_btn}>
+                            <button type='button' className={styles.btn}>
+                                Login
+                            </button>
+                        </div>
+                        <div className={styles.forgot_password}>
+                            <p>Remeber me</p>
+                            <span>Forgot password</span>
+                        </div>
+                        <div className={styles.google_login}>
+                            <img src={logogoogle}></img>
+                        </div>
+                        <div className={styles.google_register}>
+                            <p>Don’t have an account?</p>
+                            <span>Register</span>
+                        </div>
+                    </form>
+                </div>
+               </div>
            </div>
            <div className={`${styles.form_section} ${mediastyles.logindireito}`} >
                 <div className={`${styles.form_container} ${mediastyles.responsive} ${mediastyles.form_section}`}>
