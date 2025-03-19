8250d99e970e1c1221b99def61c4f2e2fb1d50d4
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-19 17:20:31 -0300

    importando componentes

--- detec-anomalias/src/App.js
+++ detec-anomalias/src/App.js
@@ -1,6 +1,9 @@
 import Login from "./components/Login";
 import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
 import Dashboard from "./components/Pages/Dasboard";
+import GestaoContas from "./components/Pages/GestaoContas";
+import GestaoTransacoes from "./components/Pages/GestaoTransacoes"; 
+import Notificacoes from "./components/Pages/Notificacoes";
 function App() {
   return (
     <Router>
