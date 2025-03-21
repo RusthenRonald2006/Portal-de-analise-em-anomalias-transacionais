f6aa8eb118f7ead576b1000e10405dc7e87a1e68
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-21 14:58:33 -0300

    criando objetos para adiconar a sidebar

--- detec-anomalias/src/components/layout/Navbar.js
+++ detec-anomalias/src/components/layout/Navbar.js
@@ -2,8 +2,15 @@ import styles from "./Navbar.module.css"
 import {Link} from "react-router-dom"
 import { Home, Users, Settings, BarChart2, FileText } from 'lucide-react';
 function Navbar(){
+    const menuItems = [
+        { icon: Home, text: 'Dashboard' },
+        { icon: Users, text: 'Usuários' },
+        { icon: FileText, text: 'Relatórios' },
+        { icon: BarChart2, text: 'Análises' },
+        { icon: Settings, text: 'Configurações' },
+      ];
     return(
-        <nav className={styles.container_list}>
-            
-        </nav>
+        <div>
+
+        </div>
 )} export default Navbar
