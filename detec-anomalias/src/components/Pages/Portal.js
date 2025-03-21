ad8c26f65a6bd599da1f968b06df65b6dd8888e7
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-21 15:50:51 -0300

    importando icone de menu

--- detec-anomalias/src/components/Pages/Portal.js
+++ detec-anomalias/src/components/Pages/Portal.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import Dashboard from "./Dashboard"
 import styles from "./Portal.module.css"
 import Sidebar from "../layout/Sidebar"
+import { Menu } from 'lucide-react';
 function Portal(){
     return(
         <div className={StyleSheet.app_container}>
             <button className={styles.mobile_menu_button}>
-
+                <Menu/>
             </button>
             <div></div>
             <div className={`${styles.sidebar} ${styles.sidebarOpen ? 'open' : ''}`}>
