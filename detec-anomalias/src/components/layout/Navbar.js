0f986777f80e4aeebc45632d747725b2eb66254e
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-21 14:57:09 -0300

    importando icones na navbar

--- detec-anomalias/src/components/layout/Navbar.js
+++ detec-anomalias/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import styles from "./Navbar.module.css"
 import {Link} from "react-router-dom"
+import { Home, Users, Settings, BarChart2, FileText } from 'lucide-react';
 function Navbar(){
     return(
         <nav className={styles.container_list}>
