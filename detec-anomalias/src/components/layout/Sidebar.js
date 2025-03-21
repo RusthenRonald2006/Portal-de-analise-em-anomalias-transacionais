c8afb5705093bb142392a37ce5d12522dd0f8a4d
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-21 17:10:01 -0300

    deletei item do sidebar desnecessario

--- detec-anomalias/src/components/layout/Sidebar.js
+++ detec-anomalias/src/components/layout/Sidebar.js
@@ -3,8 +3,7 @@ import {Link} from "react-router-dom"
 import { Home, Users, Settings, BarChart2, FileText } from 'lucide-react';
 function Navbar(){
     const menuItems = [
-        { icon: Home, text: 'Dashboard' },
-        { icon: Users, text: 'Contas Suspeitas' },
+        { icon: Home, text: 'Dashboard' , path: '/' },
         { icon: FileText, text: 'Gestão de Transações' },
         { icon: BarChart2, text: 'Gestão de Contas' },
         { icon: Settings, text: 'Tela de Notificações' },
