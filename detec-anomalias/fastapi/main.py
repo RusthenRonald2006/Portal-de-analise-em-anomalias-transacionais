015294023ec349a91f49c6f720f1515ca56861d6
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-26 01:04:33 -0300

    permitindo resquisições entre o front e o back

--- detec-anomalias/fastapi/main.py
+++ detec-anomalias/fastapi/main.py
@@ -1,2 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+#Adiciona o middleware CORS para permitir requisições entre frontend (React) e backend (FastAPI).
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],#Permite apenas requisições do React, que roda nessa porta.
+    allow_credentials=['*'],
+    allow_methods=['*'], #permite qualquer metodo http
+    allow_headers=['*']
+)
+
+@app.get('/transacoes') #define uma rota get
