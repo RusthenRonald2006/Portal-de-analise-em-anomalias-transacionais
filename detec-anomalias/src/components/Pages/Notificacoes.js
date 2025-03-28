077f481c4261e54083aa285a1161c370167da928
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-28 17:44:14 -0300

    importando sidebar na tela de notificações

--- detec-anomalias/src/components/Pages/Notificacoes.js
+++ detec-anomalias/src/components/Pages/Notificacoes.js
@@ -3,9 +3,21 @@ import styles from "./Notificacoes.module.css"
 import Sidebar from "../layout/Sidebar"
 import { Menu } from 'lucide-react';
 function Notificacoes(){
+
+    const [sidebarOpen,setSidebarOpen] = useState(false)
+
     return(
-        <div>
-            <h1>Notificações</h1>
+        <div className={styles.app_container}>
+            <button className={styles.mobile_menu_button} onClick={()=> setSidebarOpen(!sidebarOpen)}>
+                <Menu/>
+            </button>
+            <div className={`${styles.overlay} ${sidebarOpen ? styles.open : ''}`}  onClick={() => setSidebarOpen(false)}/>
+            <div className={`${styles.sidebar} ${sidebarOpen ?styles.open : ''}`}>
+                <Sidebar/>
+            </div>
+            <div className={styles.main_content}>
+                <h1>Notificações</h1>
+            </div>
         </div>
     )
 } export default Notificacoes
