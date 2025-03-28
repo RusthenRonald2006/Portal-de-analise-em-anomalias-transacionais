9d150982f75f0040a96df85f6a958124aaf7483b
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-28 16:55:46 -0300

    adicionando estilos e componente sidebar na tela de gestão de transações

--- detec-anomalias/src/components/Pages/GestaoTransacoes.js
+++ detec-anomalias/src/components/Pages/GestaoTransacoes.js
@@ -4,10 +4,10 @@ function GestaoTransacoes(){
     return(
         <div className={styles.app_container}>
             <button></button>
-            <div className={StyleSheet.sidebar}>
-                <h2>Sidebar</h2>
+            <div className={styles.sidebar}>
+                <Sidebar/>
             </div>
-            <div className={StyleSheet.main_content}>
+            <div className={styles.main_content}>
                 <h1>Gestão Transações</h1>
             </div>
         </div>
