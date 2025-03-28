d84a9e7ec3fe491c45064043f8a8f0ef4893bbaa
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-28 17:36:59 -0300

    fazendo importações necessárias para tela de gestão de contas

--- detec-anomalias/src/components/Pages/GestaoContas.js
+++ detec-anomalias/src/components/Pages/GestaoContas.js
@@ -1,3 +1,7 @@
+import {useState} from "react"
+import styles from "./GestaoContas.module.css"
+import Sidebar from "../layout/Sidebar"
+import { Menu } from 'lucide-react';
 function GestaoContas(){
     return(
         <div>
