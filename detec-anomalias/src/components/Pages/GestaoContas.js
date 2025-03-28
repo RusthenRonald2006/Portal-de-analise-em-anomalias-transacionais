a9bc20a05b1bf297020e8f4dd95887ffa4105256
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-28 17:38:58 -0300

    importando sidebar e funcionalidade

--- detec-anomalias/src/components/Pages/GestaoContas.js
+++ detec-anomalias/src/components/Pages/GestaoContas.js
@@ -3,9 +3,21 @@ import styles from "./GestaoContas.module.css"
 import Sidebar from "../layout/Sidebar"
 import { Menu } from 'lucide-react';
 function GestaoContas(){
+
+    const [sidebarOpen,setSidebarOpen] = useState(false)
+
     return(
-        <div>
-            <h1>Gestão Contas</h1>
+        <div className={styles.app_container}>
+            <button className={styles.mobile_menu_button} onClick={()=> setSidebarOpen(!sidebarOpen)}>
+                <Menu/>
+            </button>
+            <div className={`${styles.overlay} ${sidebarOpen ? styles.open : ''}`}  onClick={() => setSidebarOpen(false)}/>
+            <div className={`${styles.sidebar} ${sidebarOpen ?styles.open : ''}`}>
+                <Sidebar/>
+            </div>
+            <div className={styles.main_content}>
+                <h1>Gestão Transações</h1>
+            </div>
         </div>
     )
 } export default GestaoContas
