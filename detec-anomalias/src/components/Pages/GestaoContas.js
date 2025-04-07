81ef5a5b57da3a727b7c31bd31f97ff3f2efef97
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-04-07 14:57:45 -0300

    atualizei objetos

--- detec-anomalias/src/components/Pages/GestaoContas.js
+++ detec-anomalias/src/components/Pages/GestaoContas.js
@@ -5,25 +5,33 @@ import { Menu } from 'lucide-react';
 function GestaoContas(){
     const [contas,setContas]=useState(
         [{
-            id:1,
+            id:"1",
             numero: "1234-5",
-            Titular:"Rusthe Ronald,",
+            Titular:"Rusthen Ronald,",
             TransaçõesSuspeitas : 3 ,
             ultimaAtividade:{
                 data:"2024-03-15",
                 valor: 15000.00
-            }
+            },
             Status:"suspeita"
         },{
             id: '2',
             numero: '5678-9',
             titular: 'Maria Santos',
             transacoesSuspeitas: 5,
+            ultimaAtividade: {
+                data: '2024-03-14',
+                valor: 25000.00
+            },
             status:"análise"
         },{
             id:"3",
             numero:'9012-3',
             titular:"Allan Maia ",
+            ultimaAtividade: {
+                data: '2024-03-13',
+                valor: 8000.00
+              },
             status: "normal"
         }]
     )
