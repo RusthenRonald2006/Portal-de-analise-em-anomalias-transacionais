4a9a90b833402e4634fbb43abbf5f7f4963da3ab
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-05-21 20:53:15 -0300

    exportei service

--- detec-anomalias/src/services/api.js
+++ detec-anomalias/src/services/api.js
@@ -4,3 +4,14 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "https://antifraude-api.onrender.com",
 })
+
+api.interceptors.request.use(
+    (config)=>{
+        const token = localStorage.getItem("token")
+        if (token){
+            config.headers.Authorization = `bearer ${token}`
+        } return config;
+    },
+    (error)=>Promise.reject(error)
+) 
+export default api;
