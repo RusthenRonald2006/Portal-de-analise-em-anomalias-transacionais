e2e4ea16597dfc98bca3fe3938daaf1f35b58a8a
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-05-27 15:20:26 -0300

    subsitituindo dados mockados pelos da api

--- detec-anomalias/src/components/Pages/Dashboard.js
+++ detec-anomalias/src/components/Pages/Dashboard.js
@@ -111,13 +111,15 @@ function Dashboard(){
                     <h2 className={styles.card_notifications_title}> Atividades Recentes</h2>
                     <div className={styles.activity_list}>
                         {notificacoesRecntes.map((notificacao,index)=>(
-                            <div key={notificacao._id} className={styles.activity_item}>
+                            <div key={notificacao._id || index} className={styles.activity_item}>
                                 <div className={styles.activity_icon}>
                                     <Users/>
                                 </div>
                                 <div className={styles.activity_info}>
-                                    <p>Novo usuário registrado</p>
-                                    <p>Há {index + 1} horas</p>
+                                    <p>{notificacao.mensagem}</p>
+                                    <p>{new Date(notificacao.data).toLocaleString("pt-BR", {
+                                    hour: "2-digit", minute: "2-digit", day: "2-digit", month: "2-digit"
+                                    })}</p>
                                 </div>
                             </div>
                         ))}
@@ -130,81 +132,3 @@ function Dashboard(){
         </div>
         )
 } export default Dashboard
-
-
-/*
-<p>Total: {metricas.total_transacoes}</p>
-<p>Suspeitas: {metricas.transacoes_suspeitas}</p>
-<p>Média: R$ {metricas.valor_medio_suspeitas.toFixed(2)}</p>
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* <div className={styles.sidebar}>
-<img src={logobanese} className={styles.logo_banese}></img>
-<Navbar/>
-</div>
-<div className={styles.metrics}>
-<div>
-    <h1>Visão Geral das Transações</h1>
-</div>
-<div className={styles.container_cards}>
-    <div className={styles.group_cards}>
-        <FontAwesomeIcon icon={faChartSimple} className={styles.icon} />
-        <h3>5.320 transações </h3>
-        <p>Total  de Transações
-        (Este mês)</p>
-    </div>
-    <div className={styles.group_cards}>
-        <FontAwesomeIcon icon={faEye} className={styles.icon} />
-        <h3>( 320 casos identificados) </h3>
-        <p>Transações suspeitas</p>
-    </div>
-    <div className={styles.group_cards}>
-        <FontAwesomeIcon icon={faUser} className={styles.icon} />
-        <h3>38 contas suspeitas em análise </h3>
-        <p>Total de Contas Investigadas</p>
-    </div>
-    <div className={styles.group_cards}>
-        <FontAwesomeIcon icon={faCircleXmark} className={styles.icon} />
-        <h3>12 Fraudes Confirmadas neste período</h3>
-        <p>Fraudes Confirmadas</p>
-    </div>
-    <div className={styles.group_cards}>
-        <FontAwesomeIcon icon={faCoins} className={styles.icon} />
-        <h3>R$ 2.350.000,00 </h3>
-        <p>Valor Total Movimentado</p>
-    </div>
-</div>
-<div>
-    <h1>Gráfico de Volume de Transações (Linha ou Barra)</h1>
-</div>
-<div>
-    
-</div>
-</div>*/
