e4df1e68a6423672bbe696e9dd86aed13a5482eb
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-05-29 13:55:54 -0300

    criando nova requisição

--- detec-anomalias/src/components/Pages/Dashboard.js
+++ detec-anomalias/src/components/Pages/Dashboard.js
@@ -56,6 +56,9 @@ function Dashboard(){
             return () => clearInterval(intervalo);
         },[])
         
+        const buscarNovasNotificacoes = async ()=>{
+            
+        }
 
     const getIconByRisco = (risco) =>{
         switch (risco){
