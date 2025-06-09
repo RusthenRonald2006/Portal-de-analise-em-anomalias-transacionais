2f61e27bab721cfaec15683015d6a9cebbdde0c4
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-06-09 14:55:09 -0300

    Adicionei cliploader do react spinners

--- detec-anomalias/src/components/layout/Loading.js
+++ detec-anomalias/src/components/layout/Loading.js
@@ -1,8 +1,9 @@
 import styles from "../layout/Loading.module.css";
+import {ClipLoader} from "react-spinners";
 function Loading (){
     return(
         <div className={styles.Loader_container}>
-            <div className={styles.Loader}></div>
+            <ClipLoader size={35} color="#50C878" className={styles.loader}/>
         </div>
     )
 } export default Loading;
