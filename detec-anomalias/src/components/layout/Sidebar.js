b17c1535b5448278b10aea3d04014027e62e239f
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-06-10 17:09:29 -0300

    adaptei logica de quantidade notificações recentes para ser exibida em todas as telas e deletei parte da antiga logica

--- detec-anomalias/src/components/layout/Sidebar.js
+++ detec-anomalias/src/components/layout/Sidebar.js
@@ -1,31 +1,51 @@
 import styles from "./Sidebar.module.css"
 import {Link} from "react-router-dom"
 import { Home, Users, FileText , LogOut,Bell} from 'lucide-react';
-import alan from "../img/alan.png"
-import user from "../img/user.jpg"
-import user2 from "../img/user2.jpg"
+import { useState,useEffect, use } from "react";
+import api from "../../services/api";
 function Navbar(){
+    const [qtdNovas, setQtdNovas] = useState(0);
     const menuItems = [
         { icon: Home, text: 'Dashboard' , path: '/portal' },
         { icon: FileText, text: 'Gestão de Transações',path:'/gestaotransacoes' },
         { icon: Users, text: 'Gestão de Contas' ,path:'/gestaocontas'},
-        { icon: Bell, text: 'Central de Alertas' ,path:'/notificacoes'},
+        {icon: Bell,text: 'Central de Alertas',path: '/notificacoes',isAlerta: true
+    },
         { icon:  LogOut, text : 'Logout',path:'/'}
       ];
+
+      useEffect(()=>{
+        const buscarNovas =async ()=>{
+            try{
+                const responsta = await api.get("/notificacoes");
+                const novas = responsta.data.filter((n)=> n.status === "novo")
+                setQtdNovas(novas.length);
+            } catch(error){
+                console.error("Erro ao buscar novas notificações:", error);
+            }
+      }
+        buscarNovas();
+        const intervalo = setInterval(buscarNovas, 10000); 
+        return () => clearInterval(intervalo); 
+      },[])
+
+      
+
     return(
         <>
             <div className={styles.sidebar_header}>
                 <h2 className={styles.sidebar_title}>Banese</h2>
             </div>
-            {/*<div className={styles.user_container}>
-                <img src={user2}></img>
-                <h2>José</h2>
-            </div>*/}
             <nav className={styles.sidebar_nav}>
                 {menuItems.map((item,index)=>(
                     <Link to={item.path} key={index} href="#" className={styles.nav_item}>
                         <item.icon />
-                        <span>{item.text}</span>
+                        <span >
+                            {item.text}
+                            {item.isAlerta && qtdNovas > 0 && (
+                                <span className={styles.quant_novas_not}>{qtdNovas}</span>
+                            )}
+                        </span>
                     </Link>
                 ))}
             </nav>
