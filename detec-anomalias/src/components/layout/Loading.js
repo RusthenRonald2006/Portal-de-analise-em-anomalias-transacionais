5f63cfae2e03b4c0591b83675b363a54437cf87f
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-06-10 00:09:53 -0300

    adicionando novo icone de loading do loadercircle

--- detec-anomalias/src/components/layout/Loading.js
+++ detec-anomalias/src/components/layout/Loading.js
@@ -1,8 +1,9 @@
 import styles from "../layout/Loading.module.css";
+import { LoaderCircle } from 'lucide-react'
 function Loading (){
     return(
         <>
-            
+            <LoaderCircle size={35} color="#50C878" className={styles.spinner} />
         </>
     )
 } export default Loading;
