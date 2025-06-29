8caa1be656396d308733679e8d2e52a75cd52690
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-06-29 14:49:16 -0300

    alterei nome do objeto

--- detec-anomalias/src/components/Pages/Dashboard.js
+++ detec-anomalias/src/components/Pages/Dashboard.js
@@ -211,7 +211,7 @@ function Dashboard(){
         }), icon: faChartSimple, colorClass: styles.iconPurple },
         { title: "Fraudes Confirmadas", value: "12", icon: faCircleXmark, colorClass: styles.iconRed },
         { title: "Transações não analisadas", value: metricas.transacoes_nao_analisadas.toLocaleString("pt-BR"), icon: faCircleXmark, colorClass: styles.iconRed },
-        { title: "Transações analisadas", value: `${metricas.transacoes_analisadas} (${metricas.percentual_analisadas}%)`, icon: faCircleXmark, colorClass: styles.iconRed },
+        { title: "Transações analisadas", value: `${metricas.transacoes_analisadas} (${metricas.perc_analisadas}%)`, icon: faCircleXmark, colorClass: styles.iconRed },
       ];
 
 
